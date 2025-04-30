d7c3066591ead31ae9acac5737492acf9a05f233
kushagrakatiha <kushagrakatiha123@gmail.com>
2025-04-30 22:08:02 +0530

    dfs bfs

--- trees/tree.js
+++ trees/tree.js
@@ -12,6 +12,7 @@ class Tree {
   }
 }
 
+// Non-recursive approach
 Tree.prototype.insertNode = function (data) {
   const newNode = new Node(data);
 
@@ -40,3 +41,134 @@ Tree.prototype.insertNode = function (data) {
   }
   return
 };
+
+// Deletion In Binary Tree
+Tree.prototype.delete = function (data){
+  // the root should get replaced by the value returned
+  this.root = this.deleteNode(this.root, data)
+}
+
+Tree.prototype.deleteNode = function (node, data){
+  if(node == null){
+    return null
+  }
+
+  if(data > node.data){
+    // move to right
+    node.right = this.deleteNode(node.right, data)
+  }else if(data < node.data){
+    // move to left
+    node.left = this.deleteNode(node.left, data)
+  }else{
+    // handle other three cases
+    if(node.right == null && node.left == null){
+      return null
+    }else if(node.right == null){
+      return node.left
+    }else if(node.left == null){
+      return node.right
+    }else{
+      let tempNode = this.findMin(node.right)
+      node.data = tempNode.data
+      node.right = this.deleteNode(node.right, tempNode.data)
+    }
+  }
+}
+
+Tree.prototype.findMin = function(node){
+  while(node.left !== null){
+    node = node.left
+  }
+  return node
+}
+
+
+Tree.prototype.inOrderTraversal = function(){
+  let result = []
+  this.inorder(this.root, result)
+}
+
+Tree.prototype.inorder = function(node, result){
+  if(node != null){
+    this.inorder(node.left, result)
+    result.push(node.data)
+    this.inorder(node.right, result)
+  }
+  return result
+}
+
+Tree.prototype.preOrderTraversal = function(){
+  let result = []
+  this.preOrder(this.root, result)
+}
+
+Tree.prototype.preOrder = function(node, result){
+  if(node != null){
+    result.push(node.data)
+    this.preOrder(node.left, result)
+    this.preOrder(node.right, result)
+  }
+  return result
+}
+
+Tree.prototype.postTraversal = function(){
+  let result = []
+  this.postOrder(this.root, result)
+}
+
+Tree.prototype.postOrder = function(node, result){
+  if(node != null){
+    this.postOrder(node.left, result)
+    this.postOrder(node.right, result)
+    result.push(node.data)
+  }
+  return result
+}
+
+Tree.prototype.dfs = function(root){
+  let stack = [root]
+  let values = []
+  
+  while(stack.length != 0){
+    let curr = stack.pop();
+    values.push(curr.data)
+    if(curr.right){
+      stack.push(curr.right)
+    }
+    if(curr.left){
+      stack.push(curr.left)
+    }
+  }
+  return values
+}
+
+Tree.prototype.bfs = function(root){
+  let queue = [root]
+  let values = []
+
+  while(root.length > 0){
+    let curr = queue.shift()
+    values.push(curr.data)
+    if(curr.left){
+      queue.push(curr.left)
+    }
+    if(curr.right){
+      queue.push(curr.right)
+    }
+  }
+
+  return values
+}
+
+Tree.prototype.dfsRecursive = function(root){
+  if(root == null){
+    return []
+  }
+
+  const leftVal = this.dfsRecursive(root.left)
+  const rightVal = this.dfsRecursive(root.right)
+
+  return [root.data, ...leftVal, ...rightVal]
+}
+
+
